b816b2ab2e9fc113bfb081d634e07d9680a4b799
Sebastian Londono <lallamavioleta@gmail.com>
2021-01-17 12:44:07 -0500

    working render from chrome http server

--- NombreProyecto/src/main.rs
+++ NombreProyecto/src/main.rs
@@ -1,18 +1,32 @@
 use std::net::{TcpListener, TcpStream};
-
+use std::io::prelude::*;
+use std::fs;
 
 
 fn main() {
-    //println!("Hello, world!");
-    let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
-    for stream in listener.incoming() {
-    	match stream {
-        	Ok(stream) => {
-            	println!("¡Nuevo cliente!");
-        	}
-            	Err(e) => {
-                	println!("Conexión fallida")
-            	}
+
+	let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
+
+
+    	for stream in listener.incoming() {
+        	let stream = stream.unwrap();
+
+        	handle_connection(stream);
     	}
-	}
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    //let mut buffer = [0; 256];
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("index.html").unwrap();
+    //let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    stream.write(response.as_bytes()).unwrap(); //Nos ayuda a leer la cadena bytes que estamos recibiendo.
+    stream.flush().unwrap(); //Esperará e impedirá que el programa continúe hasta que se escriban todos los bytes en la conexión.
+
+
+
+
 }
